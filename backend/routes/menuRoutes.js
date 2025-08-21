b39ef2aa9f1c367f3d028201c5b9f60b1ec48fe4
hslee15 <lhs01019@kbu.ac.kr>
2025-08-21 11:27:10 +0900

    update 0821

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Menu = require("../models/Menu");
 const router = express.Router();
 
-/** 메뉴 등록 (단일 또는 배열) */
+// 메뉴 등록
 router.post("/", async (req, res) => {
     try {
         const payload = Array.isArray(req.body) ? req.body : [req.body];
@@ -17,13 +17,15 @@ router.post("/", async (req, res) => {
     }
 });
 
-/** 목록(투표수 내림차순) */
+
+
+// 목록
 router.get("/", async (_req, res) => {
     const menus = await Menu.find().sort({ votes: -1, _id: 1 });
     res.json(menus);
 });
 
-/** 투표 +1 */
+// 투표 +1
 router.post("/:id/vote", async (req, res) => {
     try {
         const updated = await Menu.findByIdAndUpdate(
@@ -38,19 +40,39 @@ router.post("/:id/vote", async (req, res) => {
     }
 });
 
-/** 결과(= 전체 정렬) */
+//투표 -1 그리고 0이하 방지
+router.post("/:id/unvote", async (req, res) => {
+    try {
+        const updated = await Menu.findByIdAndUpdate(
+        req.params.id,
+        { $inc: { votes: -1 } },
+        { new: true }
+        );
+        if (!updated) return res.status(404).json({ error: "메뉴를 찾을 수 없습니다." });
+
+        if(updated.votes < 0){
+            updated.votes = 0;
+            await updated.save();
+        }
+
+        res.json(updated);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+// 결과
 router.get("/results", async (_req, res) => {
     const results = await Menu.find().sort({ votes: -1, _id: 1 });
     res.json(results);
 });
 
-/** 1등 뽑기 (동점이면 전부 반환) */
-router.get("/top", async (_req, res) => {
-    const top = await Menu.find().sort({ votes: -1 }).limit(1);
-    const topVotes = top[0]?.votes ?? 0;
-  if (topVotes === 0) return res.json([]);      // 아직 투표 없음
-    const ties = await Menu.find({ votes: topVotes }).sort({ _id: 1 });
-  res.json(ties); // 동점이면 여러 개
-});
+// 1등 뽑기
+// router.get("/top", async (_req, res) => {
+//     const top = await Menu.find().sort({ votes: -1 }).limit(1);
+//     const topVotes = top[0]?.votes ?? 0;
+//   if (topVotes === 0) return res.json([]);      // 아직 투표 없음
+//     const ties = await Menu.find({ votes: topVotes }).sort({ _id: 1 });
+//   res.json(ties); // 동점이면 여러 개
+// });
 
 module.exports = router;
